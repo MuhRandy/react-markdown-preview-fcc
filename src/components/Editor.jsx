cd82be38ee02b84175fa7fd9fb0abcffa8f7cca5
MuhRandy <muhammadrandy0027@gmail.com>
2023-10-30 19:06:59 +0700

    update responsive final(?)

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,7 @@
-import { IconEdit } from '@tabler/icons-react';
-import Header from './Header';
-import clsx from 'clsx';
-import { cn } from '../utils';
+import { IconEdit } from "@tabler/icons-react";
+import Header from "./Header";
+import clsx from "clsx";
+import { cn } from "../utils";
 
 const Editor = ({
   maximizeEditor,
@@ -13,7 +13,7 @@ const Editor = ({
 }) => {
   return (
     <div
-      className={clsx('shadow-sm shadow-black', {
+      className={clsx("shadow-sm shadow-black", {
         hidden: maximizePreview,
       })}
     >
@@ -21,13 +21,24 @@ const Editor = ({
         handleClick={handleClick}
         maximize={maximizeEditor}
         icon={<IconEdit className="absolute left-0" />}
-        text={'Editor'}
+        text={"Editor"}
       />
       <textarea
-        className={cn('min-w-[100vw] px-1 min-h-[40vh]', 'sm:min-w-[90vw]', {
-          'min-h-[95vh]': maximizeEditor,
-          'sm:min-w-[50vw]': sideBySide,
-        })}
+        className={cn("min-w-[100vw] px-1 min-h-[40vh]", [
+          [
+            "sm:min-w-[90vw] sm:px-3",
+            {
+              "sm:min-w-[45vw] min-h-[221vh]": sideBySide && !maximizeEditor,
+            },
+          ],
+          [
+            "md:min-w-[60vw] md:min-h-[50vh]",
+            {
+              "md:min-w-[45vw] md:min-h-[223vh]": sideBySide && !maximizeEditor,
+              "min-h-[95vh] md:min-h-[95vh] md:min-w-[70vw]": maximizeEditor,
+            },
+          ],
+        ])}
         name="editor"
         id="editor"
         onChange={handleChange}
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,16 +1,18 @@
-import { IconBlockquote } from '@tabler/icons-react';
-import Header from './Header';
-import clsx from 'clsx';
+import { IconBlockquote } from "@tabler/icons-react";
+import Header from "./Header";
+import clsx from "clsx";
+import { cn } from "../utils";
 
 const Preview = ({
   maximizeEditor,
   maximizePreview,
+  sideBySide,
   markdown,
   handleClick,
 }) => {
   return (
     <div
-      className={clsx('shadow-sm shadow-black', {
+      className={clsx("shadow-sm shadow-black", {
         hidden: maximizeEditor,
       })}
     >
@@ -18,25 +20,34 @@ const Preview = ({
         handleClick={handleClick}
         maximize={maximizePreview}
         icon={<IconBlockquote className="absolute left-0" />}
-        text={'Preview'}
+        text={"Preview"}
       />
       <div
         id="preview"
         dangerouslySetInnerHTML={{ __html: markdown }}
-        className={clsx(
-          'prose prose-sm min-w-[100vw] p-1 leading-5 bg-slate-200',
-          ['sm:prose-base sm:min-w-[90vw] sm:px-4', 'md:prose-lg'],
+        className={cn(
+          "prose prose-sm min-w-[100vw] p-1 leading-5 bg-slate-200",
           [
-            'prose-headings:border-b prose-headings:border-black',
-            'prose-p:whitespace-pre-wrap',
             [
-              'prose-table:max-w-fit',
-              'prose-td:border-black prose-td:border prose-td:p-1',
-              'prose-th:border-black prose-th:border prose-th:p-1',
+              "sm:prose-base sm:min-w-[90vw] sm:px-4",
+              { "sm:min-w-[40vw] sm:px-1": sideBySide && !maximizePreview },
             ],
-            'prose-blockquote:border-l-4 prose-blockquote:border-slate-500 prose-blockquote:ml-3',
-            'prose-pre:p-0',
-            'marker:text-black',
+            [
+              "md:prose-lg md:min-w-[70vw]",
+              { "md:min-w-[53vw]": sideBySide && !maximizePreview },
+            ],
+          ],
+          [
+            "prose-headings:border-b prose-headings:border-black",
+            "prose-p:whitespace-pre-wrap",
+            [
+              "prose-table:max-w-fit",
+              "prose-td:border-black prose-td:border prose-td:p-1",
+              "prose-th:border-black prose-th:border prose-th:p-1",
+            ],
+            "prose-blockquote:border-l-4 prose-blockquote:border-slate-500 prose-blockquote:ml-3",
+            "prose-pre:p-0",
+            "marker:text-black",
           ]
         )}
       />
